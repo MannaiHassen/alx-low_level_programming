d7f9b87d79f03b0a0064222a19b1b09742ba9c2b
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-11 00:49:25 +0200

    Task 5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+lude <stdio.h>
+/**
+ * *main - Entry point of the program
+ * *
+ * *Return: 0 always
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+~                                                                                 ~                                                                                 ~                                                                                 ~                                                                                 ~                                                                                 ~                                                
+~                                                                                 ~                                                                                 ~                                                                                 ~                                                                                 ~                                                                                 ~                                                        
