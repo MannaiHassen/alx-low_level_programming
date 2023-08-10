ec205dc08650325b29e6c38db50fdb0038c15871
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-11 00:31:56 +0200

    Task puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+/*
+ *Puts
+ */
 int main(void)
-
 {
-        puts ("\"Programming is like building a multilingual puzzle") ; 
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
