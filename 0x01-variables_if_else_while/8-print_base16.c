abeeff5e92f56bf33558727e370ac49acb556421
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-12 03:25:36 +0200

    Taks8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * *main - Entry point of the program
- * *Description:'all numbers of base 16 in lowercase,putchar three times maximum'
- * *Return: always 0
- * */
+*main - Entry point of the program
+*Description:'numbers of base 16 in lowercase,putchar three times maximum'
+*Return: always 0
+*/
 int main(void)
 {
 int n = 48;
