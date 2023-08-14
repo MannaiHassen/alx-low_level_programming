30a6137a500c8197abc4d1400e391cda54b782ce
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-14 22:47:23 +0200

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,20 @@
 int main(void)
 {
 int i, j;
-for (i = 48; i <57; i++)
+for (i = 48; i < 57; i++)
 {
-for (j = i+1; j <=57 ; j++)
+for (j = i + 1; j <= 57 ; j++)
 {
 putchar(i);
 putchar(j);
 
-if (i == 56 && j ==57)
+if (i == 56 && j == 57)
 {
-putchar(' ');	
 continue;
 }
 putchar(',');
 putchar(' ');
 }}
+putchar('\n');
 return (0);
 }
