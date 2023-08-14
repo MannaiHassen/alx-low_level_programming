8af41c5e7aa3715de402382d33965326472bc8a5
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-14 19:28:21 +0200

    Task number 10 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main: the entry point of the program
+* Description: this algorithm print all combinations of 2 dgits
+* Return: always 0
+*/
+int main(void)
+{
+int i, j;
+for (i = 48; i <57; i++)
+{
+for (j = i+1; j <=57 ; j++)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j ==57)
+{
+continue;
+}
+putchar(' ');
+putchar(',');
+}}
+return (0);
+}
