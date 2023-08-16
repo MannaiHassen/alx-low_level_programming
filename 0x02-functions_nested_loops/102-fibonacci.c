e32665d697009acedc3e760b36347121109d531b
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-16 20:24:15 +0200

    task 15 done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - printing all firt 50 Fibonacci numbers
+ * Return: always 0
+ */
+long int main(void)
+{
+long int n, first, second, next;
+int i;
+
+first = 1;
+second = 2;
+printf("%ld, %ld, ", first, second);
+for (i = 3; i <= 50; i++)
+{
+next = first + second;
+if (i != 50)
+{
+printf("%ld, ", next);
+}
+else
+{
+printf("%ld\n", next);
+}
+first = second;
+second = next;
+}
+}
