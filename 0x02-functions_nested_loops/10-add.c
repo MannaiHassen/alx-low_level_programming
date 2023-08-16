7dbb3d6fd9e3b9d253fcb188bcd04c06cd97a532
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-16 17:22:14 +0200

    task 11 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adding two integers
+ * @a: first integer to add
+ * @b: second integer to add
+ * Return: The result of adding two integers
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+lude "main.h"
+
+/**
+ * times_table - This function print the 9 times table
+ *
+ * Return: It's a void
+ */
+void times_table(void)
+{
+for (int i = 0; i < 10; i++)
+{
+for (int j = 0; j < 10; j++)
+{
+	_putchar('0' + i * j);
+	if (j != 9)
+	{
+
+	}
+}
+}
+}
