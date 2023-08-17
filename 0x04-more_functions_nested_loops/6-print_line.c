4b7ada34b19b397edc97a9178dc55ac48d73e94e
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-17 13:54:39 -0700

    Task 7 done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times of the  the character _
+ */
+void print_line(int n)
+{
+	int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
