f43eef1a0815ed0ee13d6440822799c3841798d7
MannaiHassen <mannai.hassen.dev@gmail.com>
2023-08-21 10:13:25 -0700

    Task 5 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev -  function that returns the inverse of a string
+ * @s: a string
+ * Return: It's a void
+ */
+
+void print_rev(char *s)
+{
+int i;
+for (i = strlen(s); i > -1 ; i--)
+{
+_putchar(s[i]);
+
+}
+_putchar('\n');
+}
